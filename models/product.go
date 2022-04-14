16531b22690da3993a9c597ee4f6212a210ffaa9
Andrew Batallones <batallonesa@gmail.com>
2022-04-14 10:11:03 -0700

    added product model implementation

--- models/product.go
+++ models/product.go
@@ -2,31 +2,60 @@ package models
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type product struct {
-	ID    int     `json:"id`
+	ID    int     `json:"id"`
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
 
-func (p *product) getProducts(db *sql.DB) error {
-	return errors.New("Not Implmemented")
+func (p *product) createProduct(db *sql.DB) error {
+	err := db.QueryRow("INSERT INTO products(name, price) VALUES($1, $2) RETURNING id", p.Name, p.Price).Scan(&p.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *product) getProduct(db *sql.DB) error {
+	return db.QueryRow("SELECT name, price FROM products WHERE id=$1", p.ID).Scan(&p.Name, &p.Price)
 }
 
 func (p *product) updateProduct(db *sql.DB) error {
-	return errors.New("Not Implmemented")
+	_, err := db.Exec("UPDATE products SET name=$1, price=$2 WHERE id=$3", p.Name, p.Price, p.ID)
+
+	return err
 }
 
 func (p *product) deleteProduct(db *sql.DB) error {
-	return errors.New("Not Implmemented")
-}
+	_, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
 
-func (p *product) createProduct(db *sql.DB) error {
-	return errors.New("Not Implmemented")
+	return err
 }
 
 func getProducts(db *sql.DB, start, count int) ([]product, error) {
-	return nil, errors.New("Not Implmemented")
+	rows, err := db.Query("SELECT id, name, price FROM products LIMIT $1 OFFSET $2", count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// defer just means we execute this piece of code at the end of the block
+	// useful when wanting to close a file after editing it
+	defer rows.Close()
+
+	products := []product{}
+
+	for rows.Next() {
+		var p product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+
+	return products, nil
 }
